608cd8703bf5bd500a7e16b9463f863045da0e27
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-27 22:11:16 -0800

    impl PostRemoteEvent

--- lib/features/feature_get_posts/presentation/bloc/remote_post_event.dart
+++ lib/features/feature_get_posts/presentation/bloc/remote_post_event.dart
@@ -1,6 +1,10 @@
 part of 'remote_post_bloc.dart';
 
 
-sealed class PostRemoteEvent  {
-
+sealed class PostRemoteEvent extends Equatable {
+  const PostRemoteEvent();
+  @override
+  List<Object?> get props => [];
 }
+
+class OnRemotePostEvent extends PostRemoteEvent {}
